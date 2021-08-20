1ccdbe831abcfcdd28e462f5cb205fc265c65239
Benjamin Efenudu <benjaminefenudu@gmail.com>
2021-08-20 16:34:44 +0100

    enable auth, disable view engine

--- index.js
+++ index.js
@@ -12,18 +12,18 @@ console.log("...waiting for database connection...");
 app.use(express.json());
 app.use(cookieParser()); // Use cookies to set access token
 app.use(cors());
-app.use(express.urlencoded({ extended: true }));
-app.use("/uploads/resume", express.static("public/uploads/resume"));
+// app.use(express.urlencoded({ extended: true }));
+// app.use("/uploads/resume", express.static("public/uploads/resume"));
 
-// // Import Authorization Middleware
-// const authorize = require("./src/middlewares/authorize");
+// Import Authorization Middleware
+const authorize = require("./src/middlewares/authorize");
 
 // import routes
-const jobBasicsRoutes = require("./src/routes/jobBasic");
+const jobBasicRoutes = require("./src/routes/jobBasic");
 const employerBasicRoutes = require("./src/routes/employerBasic");
 const employerAdminRoutes = require("./src/routes/employerAdmin");
 
 // Set base routes
-app.use("/", jobBasicsRoutes);
+app.use("/", jobBasicRoutes);
 app.use("/employer", employerBasicRoutes);
-app.use("/employer", employerAdminRoutes);
+app.use("/employer", authorize.protect, employerAdminRoutes);
