910612a309d1da4d43788f9fb7fa8eb9aeef202c
Benjamin Efenudu <benjaminefenudu@gmail.com>
2021-08-20 19:27:36 +0100

    working production environment

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const database = require("./config/database.js");
 
-// // Import Authorization Middleware
-const authorize = require("./src/middlewares/authorize");
-
 // fetch database, app listen
 database.connectDatabase(app);
 console.log("...waiting for database connection...");
 
 // body parser, cors
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser()); // Use cookies to set access token
-app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 // app.use("/uploads/resume", express.static("public/uploads/resume"));
+
+// // Import Authorization Middleware
+const authorize = require("./src/middlewares/authorize");
 
 // import routes
 const jobBasicRoutes = require("./src/routes/jobBasic");
@@ -25,5 +25,5 @@ const employerAdminRoutes = require("./src/routes/employerAdmin");
 
 // Set base routes
 app.use("/", jobBasicRoutes);
-app.use("/employer", employerBasicRoutes);
+app.use("/account", employerBasicRoutes);
 app.use("/employer", authorize, employerAdminRoutes);
