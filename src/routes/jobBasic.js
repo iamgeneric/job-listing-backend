eefd43271a41a57f5fd6b519604914ad415501a6
Benjamin Efenudu <benjaminefenudu@gmail.com>
2021-08-21 14:38:19 +0100

    fix: change selectJobPost route

--- src/routes/jobBasic.js
+++ src/routes/jobBasic.js
@@ -6,7 +6,7 @@ const jobBasic = require("../controllers/jobBasic");
 // unprotected routes
 router.get("/", jobBasic.browseJobPosts);
 router.get("/search/query", jobBasic.searchJobPosts); // location or keyword
-router.get("/job/:id", jobBasic.selectJobPost);
+router.get("/:id", jobBasic.selectJobPost);
 router.post("/apply/:id", uploadResume, jobBasic.applyForJob);
 
 module.exports = router;
